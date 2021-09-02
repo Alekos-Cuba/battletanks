55b3eabc5af719c6f161dfb1aba20daa6f87a1cf
Alekos <alekosmp86@gmail.com>
2021-09-02 19:36:05 -0400

    Added clickable tile

---
+++ src/components/ClickableTile.js
@@ -0,0 +1,32 @@
+import "./../css/clickableTile.css";
+import { useState } from "react";
+import gameManager from "../scripts/GameManager";
+
+function ClickableTile(props) {
+  const [tileClicked, setTileClicked] = useState(false);
+  const [tileHasUnit, setTileHasUnit] = useState(false);
+
+  const handleClick = () => {
+    setTileClicked(true);
+    if (gameManager.hasUnitAtCoordinates(props.coords, gameManager.aiUnits)) {
+      setTileHasUnit(true);
+    } else {
+      setTileHasUnit(false);
+    }
+  };
+
+  return (
+    <div
+      className={
+        tileClicked
+          ? tileHasUnit
+            ? "tile-unit-destroyed"
+            : "tile-unit-miss"
+          : "tile-clickable"
+      }
+      onClick={handleClick}
+    ></div>
+  );
+}
+
+export default ClickableTile;
