6bb84e29973aa76e4f3470348733a3c6bc8643b2
Alekos <alekosmp86@gmail.com>
2021-09-02 20:49:54 -0400

    Fixed player's tile

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,39 +1,11 @@
 import "./../css/tile.css";
-import gameManager from "../scripts/GameManager";
 
 function Tile(props) {
   const getTileClass = () => {
-    return props.selectable ? "tile-selectable" : "tile";
+    return props.hasUnit ? "tile-player-unit" : "";
   };
 
-  const getTileImageClass = () => {
-    return props.hasUnit ? "" : "tile-hidden";
-  };
-
-  const getTileUnitType = () => {
-    return props.unitType === gameManager.unitTypes.Player
-      ? "tile-player-unit"
-      : "tile-ai-unit";
-  };
-
-  const revealTile = () => {
-    if (props.selectable) {
-      console.log(
-        gameManager.hasUnitAtCoordinates(
-          props.coords,
-          gameManager.aiUnitsDistribution
-        )
-      );
-    }
-  };
-
-  return (
-    <div className={getTileClass()} onClick={revealTile}>
-      <div className={getTileImageClass()}>
-        <div className={`tile-image ${getTileUnitType()}`}></div>
-      </div>
-    </div>
-  );
+  return <div className={`tile ${getTileClass()}`}></div>;
 }
 
 export default Tile;
